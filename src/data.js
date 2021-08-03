071d351679e6c1afa1888ae80bf5936eb3494358
Dimitar Petrov <60468978+DimitarPetrov1@users.noreply.github.com>
2021-08-03 15:38:22 +0300

    added counter for winnings

--- src/data.js
+++ src/data.js
@@ -11,8 +11,8 @@ const orange =
 const plum =
   "https://raw.githubusercontent.com/DimitarPetrov1/slots/d018f084a0691c850e55c2e18a89212d2486cf15/src/img/plum.png";
 const watermelon =
-  "https://raw.githubusercontent.com/DimitarPetrov1/slots/d018f084a0691c850e55c2e18a89212d2486cf15/src/img/seven.png";
-const seven =
   "https://raw.githubusercontent.com/DimitarPetrov1/slots/d018f084a0691c850e55c2e18a89212d2486cf15/src/img/watermelon.png";
+const seven =
+  "https://raw.githubusercontent.com/DimitarPetrov1/slots/d018f084a0691c850e55c2e18a89212d2486cf15/src/img/seven.png";
 
 export { apple, cherry, grapes, lemon, orange, plum, watermelon, seven };
--- src/index.js
+++ src/index.js
@@ -20,79 +20,42 @@ let COL1 = document.getElementById("col1");
 let COL2 = document.getElementById("col2");
 let COL3 = document.getElementById("col3");
 
-COL1.innerHTML = `${generateColumn().map((i) => {
-  if (i === 0) {
-    return `<img src='${apple}' alt='${i}'>`;
-  } else if (i === 1) {
-    return `<img src='${cherry}' alt='${i}'>`;
-  } else if (i === 2) {
-    return `<img src='${grapes}' alt='${i}'>`;
-  } else if (i === 3) {
-    return `<img src='${lemon}' alt='${i}'>`;
-  } else if (i === 4) {
-    return `<img src='${orange}' alt='${i}'>`;
-  } else if (i === 5) {
-    return `<img src='${plum}' alt='${i}'>`;
-  } else if (i === 6) {
-    return `<img src='${watermelon}' alt='${i}'>`;
-  } else if (i === 7) {
-    return `<img src='${seven}' alt='${i}'>`;
-  } else {
-    return i;
-  }
-})}`;
-COL2.innerHTML = `${generateColumn().map((i) => {
-  if (i === 0) {
-    return `<img src='${apple}' alt='${i}'>`;
-  } else if (i === 1) {
-    return `<img src='${cherry}' alt='${i}'>`;
-  } else if (i === 2) {
-    return `<img src='${grapes}' alt='${i}'>`;
-  } else if (i === 3) {
-    return `<img src='${lemon}' alt='${i}'>`;
-  } else if (i === 4) {
-    return `<img src='${orange}' alt='${i}'>`;
-  } else if (i === 5) {
-    return `<img src='${plum}' alt='${i}'>`;
-  } else if (i === 6) {
-    return `<img src='${watermelon}' alt='${i}'>`;
-  } else if (i === 7) {
-    return `<img src='${seven}' alt='${i}'>`;
-  } else {
-    return i;
-  }
-})}`;
-COL3.innerHTML = `${generateColumn().map((i) => {
-  if (i === 0) {
-    return `<img src='${apple}' alt='${i}'>`;
-  } else if (i === 1) {
-    return `<img src='${cherry}' alt='${i}'>`;
-  } else if (i === 2) {
-    return `<img src='${grapes}' alt='${i}'>`;
-  } else if (i === 3) {
-    return `<img src='${lemon}' alt='${i}'>`;
-  } else if (i === 4) {
-    return `<img src='${orange}' alt='${i}'>`;
-  } else if (i === 5) {
-    return `<img src='${plum}' alt='${i}'>`;
-  } else if (i === 6) {
-    return `<img src='${watermelon}' alt='${i}'>`;
-  } else if (i === 7) {
-    return `<img src='${seven}' alt='${i}'>`;
-  } else {
-    return i;
-  }
-})}`;
+COL1.innerHTML = `
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>`;
+COL2.innerHTML = `
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>`;
+COL3.innerHTML = `
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>
+<img src='${seven}' alt='${7}'>`;
 
 // Show balance + bet
 const targetBalance = document.getElementById("balance");
-targetBalance.textContent = BALANCE.toFixed(2);
+targetBalance.textContent = BALANCE;
 const betDisplay = document.getElementById("bet");
 betDisplay.textContent = currentBet;
+const wonDisplay = document.getElementById("won");
+// wonDisplay.textContent = 0;
 // buttons
 
 const playButton = document.getElementById("play");
 playButton.addEventListener("click", () => {
+  if (currentBet > BALANCE) {
+    return;
+  }
   playButton.disabled = true;
   lowerBet.disabled = true;
   raiseBet.disabled = true;
@@ -127,6 +90,8 @@ function animate() {
 }
 //
 function winningCombinations() {
+  let wonAmount = 0;
+
   // Check if last 3 numbers in the columns match
   COL1.innerHTML = `${generateColumn().map((i) => {
     if (i === 0) {
@@ -191,6 +156,7 @@ function winningCombinations() {
       return i;
     }
   })}`;
+
   // Since there are commas the index is incramented + 1
   let R1C1 = COL1.children[0].getAttribute("alt");
   let R1C2 = COL2.children[0].getAttribute("alt");
@@ -202,42 +168,89 @@ function winningCombinations() {
   let R3C2 = COL2.children[2].getAttribute("alt");
   let R3C3 = COL3.children[2].getAttribute("alt");
 
+  let R1C1i = COL1.children[0];
+  let R1C2i = COL2.children[0];
+  let R1C3i = COL3.children[0];
+  let R2C1i = COL1.children[1];
+  let R2C2i = COL2.children[1];
+  let R2C3i = COL3.children[1];
+  let R3C1i = COL1.children[2];
+  let R3C2i = COL2.children[2];
+  let R3C3i = COL3.children[2];
+
   // Starting with dec the balance
-  BALANCE--;
-  targetBalance.textContent = BALANCE.toFixed(2);
+  BALANCE -= currentBet;
+  targetBalance.textContent = BALANCE;
   // rows
   if (R1C1 === R1C2 && R1C2 === R1C3) {
-    console.log("first row wins");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R1C1i.classList.add("winning-img");
+      R1C2i.classList.add("winning-img");
+      R1C3i.classList.add("winning-img");
+    }, delay);
   }
   if (R2C1 === R2C2 && R2C2 === R2C3) {
-    console.log("second row wins");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R2C1i.classList.add("winning-img");
+      R2C2i.classList.add("winning-img");
+      R2C3i.classList.add("winning-img");
+    }, delay);
   }
   if (R3C1 === R3C2 && R3C2 === R3C3) {
-    console.log("third row wins");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R3C1i.classList.add("winning-img");
+      R3C2i.classList.add("winning-img");
+      R3C3i.classList.add("winning-img");
+    }, delay);
   }
   // // X shape
   if (R1C1 === R2C2 && R2C2 === R3C3) {
-    console.log("/ reversed ");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R1C1i.classList.add("winning-img");
+      R2C2i.classList.add("winning-img");
+      R3C3i.classList.add("winning-img");
+    }, delay);
   }
   if (R1C3 === R2C2 && R2C2 === R3C1) {
-    console.log("/");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R1C3i.classList.add("winning-img");
+      R2C2i.classList.add("winning-img");
+      R3C1i.classList.add("winning-img");
+    }, delay);
   }
   // ^ and v shape
   if (R1C1 === R2C2 && R2C2 === R1C3) {
-    console.log("V");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R1C1i.classList.add("winning-img");
+      R2C2i.classList.add("winning-img");
+      R1C3i.classList.add("winning-img");
+    }, delay);
   }
   if (R3C1 === R2C2 && R2C2 === R3C3) {
-    console.log("^");
-    BALANCE++;
+    BALANCE += currentBet * 2;
+    wonAmount += currentBet * 2;
+    setTimeout(() => {
+      R3C1i.classList.add("winning-img");
+      R2C2i.classList.add("winning-img");
+      R3C3i.classList.add("winning-img");
+    }, delay);
   }
+
   setTimeout(() => {
-    targetBalance.textContent = BALANCE.toFixed(2);
+    targetBalance.textContent = BALANCE;
+    wonDisplay.textContent = wonAmount;
     playButton.disabled = false;
     lowerBet.disabled = false;
     raiseBet.disabled = false;
